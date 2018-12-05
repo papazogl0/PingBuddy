e6c0a1f305b903336093b81b1f7e26e90346d8e5
Yuriy <papazogl0@yandex.ru>
2018-12-05 18:20:46 +0300

    Add files via upload

---
+++ PingBuddy/PingBuddy/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PingBuddy
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Как дела?";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(775, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 42);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.Text = "Ayyy I\'M WALKIN\' HERE!";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PingBuddy/PingBuddy/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+namespace PingBuddy
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            new Thread(() =>
+            {
+                pingthread();
+            }).Start();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        void pingthread()
+        {
+            notifyIcon1.Icon = Properties.Resources.Dead;
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            // Use the default Ttl value which is 128,
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 1000;
+            int i = 0;
+            while (true)
+            {
+                try
+                {
+                    PingReply reply;
+                    reply = pingSender.Send("8.8.8.8", timeout, buffer, options);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        timeout = 1000;
+                        if (reply.RoundtripTime < 80)
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                notifyIcon1.Icon = Properties.Resources.Good;
+                                textBox1.Text = reply.RoundtripTime.ToString();
+                                notifyIcon1.Text = reply.RoundtripTime.ToString();
+                            });
+                        if (reply.RoundtripTime > 80 && reply.RoundtripTime < 200)
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                notifyIcon1.Icon = Properties.Resources.Not_that_good;
+                                textBox1.Text = reply.RoundtripTime.ToString();
+                                notifyIcon1.Text = reply.RoundtripTime.ToString();
+                            });
+                        if (reply.RoundtripTime > 200)
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                textBox1.Text = reply.RoundtripTime.ToString();
+                                notifyIcon1.Icon = Properties.Resources.Bad;
+                                notifyIcon1.Text = reply.RoundtripTime.ToString();
+                            });
+                    }
+                    else
+                    {
+                        timeout = timeout + 1000;
+                        if (timeout > 30000) timeout = 30000;
+                        i++;
+                        textBox1.Text = "Waiting " + i.ToString() + " time to reconnect.. Current timeout:" + timeout.ToString() + reply.Status.ToString();
+                        notifyIcon1.Icon = Properties.Resources.Dead;
+                        notifyIcon1.Text = "Нет связи, ух!";
+                    }
+                    Thread.Sleep(1000);
+                }
+
+                catch (Exception ex)
+                {
+                    //Do nothing about it.
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+
+        }
+    }
+}
+    
+
