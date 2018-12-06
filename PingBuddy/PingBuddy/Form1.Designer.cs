0523ee88094982f41cb3a515d50c15d6e5cd32bb
Yuriy <papazogl0@yandex.ru>
2018-12-06 17:03:42 +0300

    Add files via upload
    Small tweaks to program's genereal behavior.

--- PingBuddy/PingBuddy/Form1.Designer.cs
+++ PingBuddy/PingBuddy/Form1.Designer.cs
@@ -46,7 +46,7 @@
             this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(775, 20);
+            this.textBox1.Size = new System.Drawing.Size(379, 20);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -54,13 +54,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(799, 42);
+            this.ClientSize = new System.Drawing.Size(403, 42);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainWindow";
             this.Opacity = 0D;
             this.ShowInTaskbar = false;
-            this.Text = "Ayyy I\'M WALKIN\' HERE!";
+            this.Text = "Ping buddy";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
--- PingBuddy/PingBuddy/Form1.cs
+++ PingBuddy/PingBuddy/Form1.cs
@@ -1,102 +1,110 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Net;
 using System.Net.NetworkInformation;
-using System.Net.Sockets;
+using System.Text;
 using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
 namespace PingBuddy
 {
     public partial class MainWindow : Form
     {
-        public MainWindow()
+        public MainWindow(string[] args)
         {
             InitializeComponent();
+            int timeout = 1000;
+            string address = "1.1.1.1";
+            if (args.Length ==1 )
+            {
+                address = args[0];
+
+            }
+            if (args.Length ==2)
+            {
+                address = args[0];
+                if (Int32.TryParse(args[1], out timeout) != true)
+                    timeout = 1000;
+            }
+            
+            Thread Simplethread = new Thread(() =>
+            {
+                while (true)
+                {
+                    pingthread(address, timeout);
+                }
+            }
+            );
+            Simplethread.Start();
         }
+
         
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Hide();
-            new Thread(() =>
-            {
-                pingthread();
-            }).Start();
+           
+            
         }
 
         private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            Environment.Exit(0);
-        }
-        void pingthread()
-        {
-            notifyIcon1.Icon = Properties.Resources.Dead;
-            Ping pingSender = new Ping();
-            PingOptions options = new PingOptions();
-
-            // Use the default Ttl value which is 128,
-            // but change the fragmentation behavior.
-            options.DontFragment = true;
-
-            // Create a buffer of 32 bytes of data to be transmitted.
-            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-            byte[] buffer = Encoding.ASCII.GetBytes(data);
-            int timeout = 1000;
-            int i = 0;
-            while (true)
+            if (this.Opacity != 0)
             {
-                try
-                {
-                    PingReply reply;
-                    reply = pingSender.Send("8.8.8.8", timeout, buffer, options);
-                    if (reply.Status == IPStatus.Success)
-                    {
-                        timeout = 1000;
-                        if (reply.RoundtripTime < 80)
-                            this.Invoke((MethodInvoker)delegate
-                            {
-                                notifyIcon1.Icon = Properties.Resources.Good;
-                                textBox1.Text = reply.RoundtripTime.ToString();
-                                notifyIcon1.Text = reply.RoundtripTime.ToString();
-                            });
-                        if (reply.RoundtripTime > 80 && reply.RoundtripTime < 200)
-                            this.Invoke((MethodInvoker)delegate
-                            {
-                                notifyIcon1.Icon = Properties.Resources.Not_that_good;
-                                textBox1.Text = reply.RoundtripTime.ToString();
-                                notifyIcon1.Text = reply.RoundtripTime.ToString();
-                            });
-                        if (reply.RoundtripTime > 200)
-                            this.Invoke((MethodInvoker)delegate
-                            {
-                                textBox1.Text = reply.RoundtripTime.ToString();
-                                notifyIcon1.Icon = Properties.Resources.Bad;
-                                notifyIcon1.Text = reply.RoundtripTime.ToString();
-                            });
-                    }
-                    else
-                    {
-                        timeout = timeout + 1000;
-                        if (timeout > 30000) timeout = 30000;
-                        i++;
-                        textBox1.Text = "Waiting " + i.ToString() + " time to reconnect.. Current timeout:" + timeout.ToString() + reply.Status.ToString();
-                        notifyIcon1.Icon = Properties.Resources.Dead;
-                        notifyIcon1.Text = "Нет связи, ух!";
-                    }
-                    Thread.Sleep(1000);
-                }
-
-                catch (Exception ex)
-                {
-                    //Do nothing about it.
-                }
+                this.Opacity = 0;
+                this.ShowInTaskbar = false;
+            }
+            else
+            {
+                this.Opacity = 100;
+                this.ShowInTaskbar = true;
             }
         }
+
+        void pingthread(string address, int timeout)
+        {
+            try
+            {
+                Ping myPing = new Ping();
+                PingReply reply = myPing.Send(address, timeout);
+                if (reply != null)
+                {
+                    
+                    if (reply.RoundtripTime < 80)
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            notifyIcon1.Icon = Properties.Resources.Good;
+                            textBox1.Text = reply.RoundtripTime.ToString()+" "+reply.Status.ToString();
+                            notifyIcon1.Text = reply.RoundtripTime.ToString();
+                        });
+                    if (reply.RoundtripTime > 80 && reply.RoundtripTime < 200)
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            notifyIcon1.Icon = Properties.Resources.Not_that_good;
+                            textBox1.Text = reply.RoundtripTime.ToString() + " " + reply.Status.ToString();
+                            notifyIcon1.Text = reply.RoundtripTime.ToString();
+                        });
+                    if (reply.RoundtripTime > 200)
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            textBox1.Text = reply.RoundtripTime.ToString() + " " + reply.Status.ToString();
+                            notifyIcon1.Icon = Properties.Resources.Bad;
+                            notifyIcon1.Text = reply.RoundtripTime.ToString();
+                        });
+                }
+            }
+            catch
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    notifyIcon1.Icon = Properties.Resources.Dead;
+                    notifyIcon1.Text = "Dead network?";
+                });
+            }
+        }
+
+       
+
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
